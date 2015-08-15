219894f4599d117b990ca23c23078c796a7135f1
Wanderson Aldo <wanderson.aldo@gmail.com>
2015-08-15 13:33:20 -0300

    Hello World
    WebPart

---
+++ WPHelloWorld.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+
+namespace WspImportProject2.WPHelloWorld
+{
+    [ToolboxItemAttribute(false)]
+    public class WPHelloWorld : WebPart
+    {
+        protected override void CreateChildControls()
+        {
+            Label lbl = new Label();
+            lbl.ID = "label1";
+            lbl.Text = "Hello World";
+
+            Controls.Add(lbl);
+        }
+    }
+}
