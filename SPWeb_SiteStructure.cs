b046f83b4e25ee9d8618b0952dc6062850f2b45f
Wanderson Aldo <wanderson.aldo@gmail.com>
2015-08-19 09:49:47 -0300

    SPWeb SiteStructure

---
+++ SPWeb_SiteStructure.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint;
+
+namespace ConsoleApplication1
+{
+    class SPWeb_SiteStructure
+    {
+        static void Main(string[] args)
+        {
+
+            using (var oSite = new SPSite("http://www.sharepointstorm.com/sites/realestate"))
+            {
+                using (var oWeb = oSite.AllWebs["residential"])
+                //using (var oWeb = oSite.OpenWeb("residential"))
+                {
+                    Console.WriteLine(oWeb.Title);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("SharePoint Storm!!!!");
+            Console.ReadLine();
+        }
+    }
+}
